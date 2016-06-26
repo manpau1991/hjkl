95a478de4e651776e10c089f7304bd2994a823a4
Thomas Lang <Langi1@gmx.at>
2016-06-27 00:37:58 +0200

    BV -> added new file extension to cleanup script

--- Bildverarbeitung/cleanup.sh
+++ Bildverarbeitung/cleanup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for file in $(find . -type f -name "*.out" -o -name "*.aux" -o -name "*.log")
+for file in $(find . -type f -name "*.out" -o -name "*.aux" -o -name "*.log" -o -name "*.toc")
 do
     printf "Removing %s ..." $file;
     rm $file;
